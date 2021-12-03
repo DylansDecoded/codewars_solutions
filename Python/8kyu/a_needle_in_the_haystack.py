4dc0319adb3b5ad111b6d47fb15ec58294a2a878
Dylan Robson <dylansdecoded@pm.me>
2021-12-02 23:08:25 -0500

    8kyu Kata solution for A Needle in the Haystack

---
+++ Python/8kyu/a_needle_in_the_haystack.py
@@ -0,0 +1,19 @@
+'''
+Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk but containing one
+"needle"
+
+After your function find the needle it should return a messege (as a string) that says"
+"found the needle at position" plus the index it found the needle
+'''
+
+def find_needle(haystack):
+    '''
+    Use the index method in python
+    to find the needles position in
+    the haystack array.
+    '''
+    
+    # Now print the required print out and index for needle
+    return 'found the needle at position {}'.format(haystack.index('needle')))
